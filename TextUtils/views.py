74755572ec3c77c3bed999388c37d5b64fd09f22
gt102005 <tayadegopalsuresh@email.com>
2025-11-10 01:30:20 +0530

    updatd on 10/11

--- TextUtils/views.py
+++ TextUtils/views.py
@@ -5,59 +5,66 @@ def index(request):
     return render(request,'index.html')
 
 def show(request):
-    texts=request.GET.get('textarea')
-    uppercase=request.GET.get('uppercase','off')
-    lowercase=request.GET.get('lowercase','off')
-    puncuations=request.GET.get('puncuations','off')
-    newlines=request.GET.get('newlines','off')
-    space_remove=request.GET.get('space_remove','off')
+    texts=request.POST.get('textarea')
+    uppercase=request.POST.get('uppercase','off')
+    lowercase=request.POST.get('lowercase','off')
+    puncuations=request.POST.get('puncuations','off')
+    newlines=request.POST.get('newlines','off')
+    space_remove=request.POST.get('space_remove','off')
     
     # Checking Empty TextArea 
     if texts=='':
         dic={'output':'You Have Not Yet Entered Anything In The Box To Analyze '}
         return render(request,'show.html',dic)
 
-    output=''
+    
     # Conversion in Uppercase
     if uppercase=='on':
-        output=texts.upper()
-        dic={'output':output}
-        return render(request,'show.html',dic)
+        texts=texts.upper()
+        # dic={'output':output}
+        # return render(request,'show.html',dic)
     
     # Conversion in Lowercase
-    elif lowercase=='on':
-        output=texts.lower()
-        dic={'output':output}
-        return render(request,'show.html',dic)
+    if lowercase=='on':
+        texts=texts.lower()
+        # dic={'output':output}
+        # return render(request,'show.html',dic)
     
     # Removing Punctuations form the Texts
-    elif puncuations=='on':
+    if puncuations=='on':
+        output=''
         punc=['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
         for i in texts:
             if i not in punc:
                 output+=i
-        dic={'output':output}
-        return render(request,'show.html',dic)
+        texts=output
+        # dic={'output':output}
+        # return render(request,'show.html',dic)
     
     # Removing Newlines for the Texts
-    elif newlines=='on':
-        for i in texts:
+    if newlines=='on':
+        output=''
+        for i in texts and texts!="/r":
             if i!="\n":
                 output+=i
-        dic={'output':output}
-        return render(request,'show.html',dic)
+        texts=output
+        # dic={'output':output}
+        # return render(request,'show.html',dic)
 
     
     # removing Space from the Texts
-    elif space_remove=='on':
+    if space_remove=='on':
+        output=''
         texts=texts.split(' ')
         for i in texts:
             output+=i+" "
-        dic={'output':output}
+        texts=output
+        # dic={'output':output}
+        # return render(request,'show.html',dic)
+    if uppercase=='on'or lowercase=="on" or puncuations=="on" or newlines=="on" or space_remove=="on":
+        dic={'output':texts}
         return render(request,'show.html',dic)
-    
     else:
-        output={'output':'Error ! you did Something Wrong...'}
-        return render(request,'show.html',)
-
+        dic={'output':'You Have Not Selected Anything To Analyze Please Select Something First ..',"error":True}
+        return render(request,"show.html",dic)
     
